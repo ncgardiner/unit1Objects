f0d3f134fd4712a376ff526ca6da2661b8b32a04
ncgardiner <ncgardiner@NN123-HA-20.HS.sd203.org>
2015-08-21 15:07:49 -0500

    TWO TURTLES!!!!!!

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -3,19 +3,35 @@ import java.awt.Color;
 public class TurtleWorld
 {
     public static void main(String args[])
-    {
+ throws InterruptedException
+{
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
+        Turtle banana = new Turtle(turtleWorld);
+        banana.setBodyColor(Color.RED);
         turtle.setBodyColor(Color.GREEN);
-        turtle.setPenColor(Color.BLUE);
-        turtle.setPenWidth(10);
-        turtle.penDown();
-        turtle.forward(50);
+        banana.setPenColor(Color.RED);
+        turtle.setPenColor(Color.GREEN);
+        banana.penUp();
+        turtle.penUp();
+        turtle.setWidth(100);
+        turtle.setHeight(100);
+        banana.setWidth(100);
+        banana.setHeight(100);
         turtle.turnRight();
-        turtle.forward(50);
-        turtle.turnRight();
-        turtle.forward(50);
-        turtle.turnRight();
-        turtle.forward(50);
+        banana.turnLeft();
+        turtle.backward(90);
+        banana.backward(90);
+        int x = 0;
+        while(x<=360)
+        {
+            turtle.turn(1);
+            turtle.forward(2);
+            banana.turn(1);
+            banana.forward(2);
+            x+=1;
+            Thread.sleep(50);
+        }
+        
     }
 }
