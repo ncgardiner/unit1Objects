385c761da04c3c4e3883af63f59d61ea46935005
ncgardiner <ncgardiner@NN123-HA-20.HS.sd203.org>
2015-08-28 15:06:22 -0500

    Chapter 2 stuff

---
+++ Chapter 2 projects BOY/TrimIt.java
@@ -0,0 +1,11 @@
+public class TrimIt
+{
+   public static void main(String[] args)
+   {
+      String sentence1 = "      abc   ";
+      String sentence2 = "   def";
+      String sentence3 = "ghi         ";
+      String message = sentence1.trim().concat(sentence2.trim()).concat(sentence3.trim());
+      System.out.println(message);
+   }
+}
